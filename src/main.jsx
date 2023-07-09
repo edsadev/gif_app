da14840a5b136856134ed5f077a44800c9f64b6c
salamancae <salamanca.e@latamdigitalmarketing.com>
2023-07-09 14:31:49 -0400

    Little bug

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from './App.jsx'
-import Home from '@/pages/home'
+import Home from './pages/home'
 import './index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch,  } from '@fortawesome/free-solid-svg-icons'
